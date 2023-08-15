01fcc1cf3658434f30f89dbe860dd62ad76a6e3a
Christopher Zeng (Deloitte) <chrzeng@deloitte.com>
2023-08-15 20:35:48 +0000

    Update dev containers

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,2 @@
 gcloud auth login --no-launch-browser && gcloud config set project us-gcp-ame-con-c2aaa-npd-1
-mamba env create -f /.devcontainer/environment.yml
-```
+mamba env create -f ./.devcontainer/environment.yml
